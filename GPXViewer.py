e27d94a7a6a66ace92c73a71dcbc2b96f49a20f6
Stefan Schinkel <mail@dreeg.org>
2014-06-15 20:16:17 +0200

    Progress

--- GPXViewer.py
+++ GPXViewer.py
@@ -14,33 +14,48 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
 
     def __init__(self, parent = None):
         
-        homeDir = unicode(os.path.expanduser("~"))
-        homeDir = QtCore.QDir.currentPath()
+        # this has to be stored in some config file
+        #dataDir = unicode(os.path.expanduser("~"))
+        dataDir = QtCore.QDir.currentPath() + '/data'
+        print dataDir
+        
         # init super and load layout
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
 
         # setup model for the file system
         self.model = QtGui.QFileSystemModel()
-        self.model.setRootPath(homeDir)
+        self.model.setRootPath(dataDir)
 
         # and a QTreeView to show it
         self.fileBrowser.setModel(self.model)
-        self.fileBrowser.setRootIndex(self.model.index(homeDir))
+        self.fileBrowser.setRootIndex(self.model.index(dataDir))
 
         #connect callbacks
         self.fileBrowser.doubleClicked.connect(self.itemDoubleClicked)
         self.fileBrowser.clicked.connect(self.itemClicked)
 
+        # Statusbar
+        self.statusbar.showMessage('')
+
     def itemClicked(self):
-        print "highlighted: %s" % (self.model.fileName(self.fileBrowser.currentIndex()))
+        f = self.model.fileName(self.fileBrowser.currentIndex())
+        print "highlighted: %s" % (f)
+
+        self.textDate.setText(f)
+        self.statusbar.showMessage(f)
 
     def itemDoubleClicked(self):
+        self.textTotalDuration.setText('1:30h')
+        self.textTotalDistance.setText('12 km')
+        self.textTotalSpeed.setText('8.8 km/h')
         print "doubleClicked"
         print self.model.fileName(self.fileBrowser.currentIndex())
         print self.model.filePath(self.fileBrowser.currentIndex())
+
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     win = MainWindow()
     win.show()
+    win.raise_()
     sys.exit(app.exec_())
