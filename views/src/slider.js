512d8c8c7c026c80af637763ee99a09948aba31e
Stefan Schinkel <mail@dreeg.org>
2016-05-31 22:30:33 +0200

    small changes in infobox

--- views/src/slider.js
+++ views/src/slider.js
@@ -1,34 +1,46 @@
 /*global $, stats, track, marker, info */
-
+/*jshint -W081 */
 $(function () {
   "use strict";
+  function formatTime(seconds) {
+    var hours = Math.floor(seconds / 3600);
+    var mins = Math.floor((seconds - (hours * 3600)) / 60);
+    var secs = seconds - (hours * 3600) - (mins * 60);
+    if (hours   < 10) {hours   = "0" + hours; }
+    if (mins < 10) {mins = "0" + mins; }
+    if (secs < 10) {secs = "0" + secs; }
+    return hours + ":" + mins + ":" + secs;
+  }
+
+
   $("#slider").slider({
     step: 1,
     max: stats.length,
     values: [1],
     slide: function (event, ui) {
-      var idx, lat, lon, d, v, time, minutes, seconds, hours, msg;
+      var idx, lat, lon, d, v, tms, msg;
       idx = ui.values[0];
-      lat = track[idx][0];
+      lat = track[idx][0];  //
       lon = track[idx][1];
-      // order is distance, duration, speed
+
+      // distance
       d = stats[idx][0] / 1000;
       d = d.toFixed(2);
-      time = stats[idx][1];
 
-      hours = Math.floor(time / 3600);
-      minutes = Math.floor((time - (hours * 3600)) / 60);
-      seconds = time - (hours * 3600) - (minutes * 60);
+      // time
+      tms = formatTime(stats[idx][1]);
 
-      if (hours   < 10) {hours   = "0" + hours; }
-      if (minutes < 10) {minutes = "0" + minutes; }
-      if (seconds < 10) {seconds = "0" + seconds; }
-      var tms = hours + ":" + minutes + ":" + seconds;
+      // speed
       v = stats[idx][2].toFixed(2);
+      if (v.length < 5) {
+        console.log(v)
+        console.log('++++')
+        v = '&nbsp;' + v;
+        console.log(v)
+        }
 
-      if (v.length < 5) { v = ' ' + v; }
 
-      msg = d + " km " + tms + "h "  + v + "km/h";
+      msg = d + "  km<br />" + tms + "  h<br> "  + v + "  km/h";
 
       // write to info box
       info.update({"time" : msg});
